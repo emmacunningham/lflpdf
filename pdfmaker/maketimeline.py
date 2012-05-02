6f47d1f84796437db5ad494a870e2b589a6bb5b0
Emma Cunningham <the.cunning.ham@gmail.com>
2012-05-01 17:29:54 -0700

    draws basic timeline graph outline

--- pdfmaker/maketimeline.py
+++ pdfmaker/maketimeline.py
@@ -44,6 +44,19 @@ reportlab.rl_config.TTFSearchPath.append( os.path.join(pdf.settings.STATIC_ROOT,
 width = 792
 height = 612
 margin = 30
+chartleft = 40 + margin
+titley = height - 50 - margin
+charttopy = titley - 50 
+rightmargin = width - margin
+useableverticalspace = charttopy - margin - 5
+useablehorizspace = rightmargin - chartleft - 30
+
+
+def prettyDateTime(datetime):
+	month = datetime.strftime('%B')
+	day = datetime.day
+	year = datetime.year
+	return "{} {}, {}".format(month, day, year)
 
 def fileDateTime(datetime):
 	month = datetime.strftime('%B')
@@ -51,29 +64,62 @@ def fileDateTime(datetime):
 	year = datetime.year
 	return "{}_{}_{}".format(month, day, year)
 
-def timelinerange(timeline):
+def timelinerange(canvas,timeline):
 	datestartall = timeline.timelinepoint_set.dates('datestart','day')
 	dateendall = timeline.timelinepoint_set.dates('dateend','day',order='DESC')
+	datestartmonth = timeline.timelinepoint_set.dates('datestart','month')
+	dateendmonth = timeline.timelinepoint_set.dates('dateend','month')
+	timelinepoints = timeline.timelinepoint_set.order_by('datestart')
+	
 	timelinefirst = datestartall[0]
 	timelinelast = dateendall[0]
-	s = fileDateTime(timelinelast)
-	return s
+	
 
-def drawlines(canvas):
-	canvas.setStrokeColorRGB(0.2,0.5,0.3)
-	canvas.line(0,0,70,70)
-	textobject = canvas.beginText()
-	textobject.setTextOrigin(51.2,749)
-	textobject.textLines('''
-	LEFT 
-	FIELD 
-	LABS
-	''')
-	canvas.drawText(textobject)
+	days = abs(timelinelast - timelinefirst)
+	projectdays = days.days
+	dayspace = useablehorizspace/projectdays
+	
+	firstmonth = timelinefirst.strftime('%B')
+	c = chartleft + 30
+	
+	x = margin + 5
+	
+	for i in range(projectdays):
+		if i%7 == 0:
+			canvas.drawString(x,-c,str(timelinefirst.day))
+			c = c + dayspace
+		else: 
+			c = c + dayspace
+		
+
+	
+
+def timelinecategories(canvas,timeline):
+	t = timeline.timelinepoint_set.all()
+	timecatset = set( )
+	for point in t:
+		timecatset.add(point.timelinecategory.categoryname)
+	tcount = len(timecatset)
+	catvertspace = useableverticalspace/tcount
+	canvas.rotate(90)
+	lefty = margin + 20
+	for timecat in timecatset:
+		canvas.drawString(lefty,-chartleft,timecat)
+		lefty = lefty + catvertspace
+
+def drawlines(canvas,timeline):
+	canvas.setStrokeColorRGB(0.0,0.0,0.0)
+	canvas.line(margin,height-margin,width-margin,height-margin)
+	canvas.drawString(chartleft,titley,'{0}'.format(timeline.project))
+	canvas.line(margin,margin,width-margin,margin)
+	canvas.drawString(margin,margin-10,'Timeline')
+	canvas.drawString(width-80,margin-10,'{0}'.format(prettyDateTime(timeline.pub_date)))
+	timelinecategories(canvas,timeline)
+	timelinerange(canvas,timeline)
+	
 
 def printpdf(timeline,timelineset):
 	filename = os.path.join(pdf.settings.MEDIA_ROOT,'pdf/timeline/{0}.pdf'.format(timeline.project))
-	c = canvas.Canvas(filename,pagesize=letter)
-	c.pagesize = landscape(letter)
-	drawlines(c)
+	c = canvas.Canvas(filename,pagesize=landscape(letter))
+	drawlines(c,timeline)
 	c.save()
